689e78fe4b2a80b124491cbee43837d4c742e7dc
chelm250 <chelmcode@outlook.com>
2025-08-21 15:07:38 +0200

    Added ClinicLocation Model

---
+++ app/src/main/java/com/project/back_end/models/ClinicLocation.java
@@ -0,0 +1,74 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class ClinicLocation {
+    
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Location name cannot be blank")
+    @Size(max = 50, message = "Location name cannot exceed 50 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Location name only can contain alphanumeric characters")
+    private String name;
+
+    @OneToOne(optional = false, cascade = CascadeType.ALL)
+    @JoinColumn(name = "location_name", referencedColumnName = "id")
+    @NotNull(message = "Address cannot be null")
+    private Address address;
+
+    // Default constructor
+    public ClinicLocation() {}
+
+    // Parametrized constructor
+    public ClinicLocation(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    // toString override
+    @Override
+    public String toString() {
+        return "ClinicLocation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';    
+    }
+
+}
